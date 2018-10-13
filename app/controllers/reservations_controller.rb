83dc515196056b7a22e3f61b598cf38f7360013d
wajima <miswndu@yahoo.co.jp>
2018-10-13 15:25:09 +0900

    refacter

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,4 +1,18 @@
 class ReservationsController < ApplicationController
+  before_action :set_room
   def create
+    @reservation = current_user.reservations.new(reservation_params.merge(room: @room))
+    if @reservation.save
+      redirect_to room_path(@room), notice: '予約を完了しました。'
+    else
+      redirect_to room_path(@room), alert: @reservation.errors.full_messages.first
+    end
+  end
+  private
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :finish_date)
+  end
+  def set_room
+    @room = Room.find(params[:room_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
-
   resources :rooms do
     resources :reservations, only: :create, module: :rooms
   end
