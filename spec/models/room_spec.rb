d44a0bc192955a4752165cc4030033a4005a1652
wajima <miswndu@yahoo.co.jp>
2018-10-14 21:37:12 +0900

    バリデーションのpresence trueをまとめてチェック ブロックを使って

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -34,22 +34,42 @@ RSpec.describe Room do
   end
 
   describe 'バリデーションテスト' do
-    it 'home_typeが空の時、保存しない' do
-      @rooms_param[:home_type] = ''
-      room = Room.create(@rooms_param)
-      expect(room.valid?).to eq(false)
+    before do
+      @presence_true_params = [:home_type,
+                               :room_type,
+                               :accommodate,
+                               :bedroom_number,
+                               :bathroom_number,
+                               :price,
+                               :title,
+                               :description,
+                               :location]
     end
 
-    it 'room_typeが空の時、保存しない' do
-      @rooms_param[:room_type] = ''
-      room = Room.create(@rooms_param)
-      expect(room.valid?).to eq(false)
-    end
+    # it 'home_typeが空の時、保存しない' do
+    #   @rooms_param[:home_type] = ''
+    #   room = @user.rooms.create(@rooms_param)
+    #   expect(room.valid?).to eq(false)
+    # end
+    #
+    # it 'room_typeが空の時、保存しない' do
+    #   @rooms_param[:room_type] = ''
+    #   room = @user.rooms.create(@rooms_param)
+    #   expect(room.valid?).to eq(false)
+    # end
+    #
+    # it 'accommodateが空の時、保存しない' do
+    #   @rooms_param[:accommodate] = ''
+    #   room = @user.rooms.create(@rooms_param)
+    #   expect(room.valid?).to eq(false)
+    # end
 
-    it 'accommodateが空の時、保存しない' do
-      @rooms_param[:accommodate] = ''
-      room = Room.create(@rooms_param)
-      expect(room.valid?).to eq(false)
+    it '入力必須項目のいずれかがからの時、保存しない' do
+      @presence_true_params.each do |presence_true_param|
+        @rooms_param[presence_true_param] = nil
+        room = @user.rooms.create(@rooms_param)
+        expect(room.valid?).to eq(false)
+      end
     end
   end
 
